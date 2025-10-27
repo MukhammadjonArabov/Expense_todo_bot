b2758c7394f7447e363130089b0cb0ac9e9967f4
MukhammadjonArabov <muhammadjonarabov04@gmail.com>
2025-10-27 15:54:11 +0500

    collective tasks

---
+++ app/keyboards/collective_keyboard.py
@@ -0,0 +1,38 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+async def get_team_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text="📂 Mening loyhalarim"),
+                KeyboardButton(text="👥 Qatnashganlarim")
+            ],
+            [
+                KeyboardButton(text="🔙 Ortga")
+            ],
+        ],
+        resize_keyboard=True,
+    )
+
+async def get_my_projects_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text="➕ Yangi loyiha"),
+                KeyboardButton(text="📝 Vazifa qo‘shish")
+            ],
+            [
+                KeyboardButton(text="🔙 Ortga")
+            ],
+        ],
+        resize_keyboard=True,
+    )
+
+async def get_joined_projects_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📁 Loyhalar ro‘yxati")],
+            [KeyboardButton(text="🔙 Ortga")],
+        ],
+        resize_keyboard=True,
+    )

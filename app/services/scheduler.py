e6cd0b1a57bb8d3366fc26edc7a829fc4f565d8a
MukhammadjonArabov <muhammadjonarabov04@gmail.com>
2025-10-30 19:07:57 +0500

    add requarments

--- app/services/scheduler.py
+++ app/services/scheduler.py
@@ -7,10 +7,10 @@ from app.services.notifications import (
 async def setup_scheduler(bot):
     """Har kuni belgilangan vaqtda xabar yuborishni yo‘lga qo‘yadi"""
 
-    @aiocron.crontab("0 6 * * *", tz="Asia/Tashkent")  # har kuni 06:00 da
+    @aiocron.crontab("0 6 * * *", tz="Asia/Tashkent")
     async def morning():
         await send_morning_notifications(bot)
 
-    @aiocron.crontab("0 20 * * *", tz="Asia/Tashkent")  # har kuni 20:00 da
+    @aiocron.crontab("0 20 * * *", tz="Asia/Tashkent")
     async def evening():
         await send_evening_notifications(bot)
